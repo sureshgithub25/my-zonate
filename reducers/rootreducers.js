f823120217a5134482ce9d2e2b7855de74b97bb2
Suresh Kumar <74665668+sureshgithub25@users.noreply.github.com>
2021-01-25 14:40:30 +0530

    Add files via upload

--- reducers/rootreducers.js
+++ reducers/rootreducers.js
@@ -1,13 +1,16 @@
-
+import res1 from './res1.jpg'
+import res2 from './res2.jpg'
+import res3 from './res3.jpg'
+import res4 from './res4.jfif'
 const initState = {
     restaurants:[
-       { id:1,name:'Appellees American' ,		location: 'Decatur' , rating:'5'	},
-       { id:2, name:"araby's Sandwiches"	, location:'Boardman', rating:'4.7'},
-       { id:3,name:'Auntie Anne'	, location:'Downtown ', rating:'4.8'},
-       { id:4, name:'Baton Rouge Steak'	, location:'Montreal', rating:'4.3'},
-       { id:5,   name:'BeaverTails	Baked'	, location:'Ottawa', rating:'4.9'},
-       { id:6, name:'Big Smoke Burger'	, location:'Toronto', rating:'4.6'},
-       { id:7, name:'Bronchos Chicken'	, location:'london', rating:'4.5'}	
+       { id:1,name:'Appellees American' ,		location: 'Decatur' , rating:'5' , image: res1	},
+       { id:2, name:"araby's Sandwiches"	, location:'Boardman', rating:'4.7',image: res3},
+       { id:3,name:'Auntie Anne'	, location:'Downtown ', rating:'4.8',image: res3},
+       { id:4, name:'Baton Rouge Steak'	, location:'Montreal', rating:'4.3',image: res4},
+       { id:5,   name:'BeaverTails	Baked'	, location:'Ottawa', rating:'4.9',image: res1},
+       { id:6, name:'Big Smoke Burger'	, location:'Toronto', rating:'4.6',image: res4},
+       { id:7, name:'Bronchos Chicken'	, location:'london', rating:'4.5',image: res3}	
     ],
     
      name: null 
